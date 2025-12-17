8b0ca171c2cf5239e825a6db949b8329dffad617
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2025-12-17 23:36:52 +0600

    Header section implemented

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,7 @@
 import {Josefin_Sans} from 'next/font/google';
 
 import '@/app/_styles/globals.css';
-import Logo from '@/app/_components/Logo';
-import Navigation from '@/app/_components/Navigation';
+import Header from '@/app/_components/Header';
 
 const josefin = Josefin_Sans({
   subsets: ['latin'],
@@ -23,10 +22,7 @@ export default function RootLayout({children}) {
   return (
     <html lang="en">
       <body className={`bg-primary-900 text-primary-100 ${josefin.className}`}>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
+        <Header />
         <main>{children}</main>
       </body>
     </html>
