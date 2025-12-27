f6f64f68c6c9fcd1880f4878353c4ddffa464049
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2025-12-27 13:34:08 +0600

    Add Cabin, DateSelector, ReservationForm, and Reservations components for cabin booking functionality

---
+++ app/_components/Reservations.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import DateSelector from './DateSelector';
+import ReservationForm from './ReservationForm';
+import {getBookedDatesByCabinId, getSettings} from '../_lib/data-service';
+
+async function Reservations({cabin}) {
+  const [settings, bookedDates] = await Promise.all([
+    getSettings(),
+    getBookedDatesByCabinId(cabin.id),
+  ]);
+
+  return (
+    <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
+      <DateSelector
+        settings={settings}
+        bookedDates={bookedDates}
+        cabin={cabin}
+      />
+      <ReservationForm cabin={cabin} />
+    </div>
+  );
+}
+
+export default Reservations;
