0602426124be5c6b98429fd7f9dee63bc370bbb3
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2026-01-01 22:51:33 +0600

    Add remote pattern for Google user content images in Next.js config

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
         port: '',
         pathname: '/storage/v1/object/public/cabins/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
