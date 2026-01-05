5568c19784a281912cacdc1f1cc4b19cbc5e9b9f
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2026-01-05 23:47:51 +0600

    Implement delete reservation functionality and update related components

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,9 @@
 'use server';
 
-const {signIn, signOut} = require('./auth');
+import {revalidatePath} from 'next/cache';
+import {deleteBooking, getBookings} from './data-service';
+
+const {signIn, signOut, auth} = require('./auth');
 
 export async function signInAction() {
   await signIn('google', {redirectTo: '/account'});
@@ -8,4 +11,17 @@ export async function signInAction() {
 
 export async function signOutAction() {
   await signOut({redirectTo: '/'});
+}
+
+// RESERVATION ACTIONS
+export async function deleteReservationAction(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error('Unauthorized');
+
+  const bookings = await getBookings(session.user.id);
+  if (!bookings.find((booking) => booking.id === bookingId))
+    throw new Error('Unauthorized');
+
+  await deleteBooking(bookingId);
+  revalidatePath('/account/reservations');
 }
