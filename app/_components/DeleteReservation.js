917aa87d0ce81bc821652a5c126c696bfd5faf00
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2026-01-06 23:03:28 +0600

    Add update reservation functionality and integrate with form; enhance delete reservation component with loading state

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -1,10 +1,16 @@
 'use client';
 import {TrashIcon} from '@heroicons/react/24/solid';
 import {deleteReservationAction} from '../_lib/actions';
+import {useTransition} from 'react';
+import SpinnerMini from './SpinnerMini';
 
 function DeleteReservation({bookingId}) {
+  const [isPending, startTransition] = useTransition();
+
   async function handleDelete() {
-    await deleteReservationAction(bookingId);
+    startTransition(async () => {
+      await deleteReservationAction(bookingId);
+    });
   }
 
   return (
@@ -12,8 +18,16 @@ function DeleteReservation({bookingId}) {
       onClick={handleDelete}
       className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
     >
-      <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
-      <span className="mt-1">Delete</span>
+      {!isPending ? (
+        <div className="flex items-center justify-center gap-2">
+          <SpinnerMini />
+        </div>
+      ) : (
+        <>
+          <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+          <span className="mt-1">Delete</span>
+        </>
+      )}
     </button>
   );
 }
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,7 +1,8 @@
 'use server';
 
 import {revalidatePath} from 'next/cache';
-import {deleteBooking, getBookings} from './data-service';
+import {deleteBooking, getBookings, updateBooking} from './data-service';
+import {redirect} from 'next/navigation';
 
 const {signIn, signOut, auth} = require('./auth');
 
@@ -14,6 +15,39 @@ export async function signOutAction() {
 }
 
 // RESERVATION ACTIONS
+export async function updateReservationAction(formData) {
+  // get session
+  const session = await auth();
+  if (!session) throw new Error('Unauthorized');
+
+  // get form data
+  const bookingId = Number(formData.get('reservationId'));
+  const numGuests = Number(formData.get('numGuests'));
+  const observations = formData.get('observations');
+  console.log({bookingId, numGuests, observations});
+
+  if (!bookingId) throw new Error('invalid id');
+
+  if (!Number.isInteger(bookingId)) {
+    throw new Error('Invalid reservation id');
+  }
+
+  if (!Number.isInteger(numGuests)) {
+    throw new Error('Invalid number of guests');
+  }
+
+  const bookings = await getBookings(session.user.id);
+  if (!bookings.find((booking) => booking.id === bookingId))
+    throw new Error('Unauthorized id');
+
+  // update the reservation
+  await updateBooking(bookingId, {numGuests, observations});
+
+  // redirect
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  redirect('/account/reservations');
+}
+
 export async function deleteReservationAction(bookingId) {
   const session = await auth();
   if (!session) throw new Error('Unauthorized');
