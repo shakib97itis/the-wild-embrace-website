0427f4bf691d63dd808c056fac5a14b6f7380a7a
Shakibul Islam <41319856+shakib97itis@users.noreply.github.com>
2026-01-12 22:21:36 +0600

    Replace logo image with new gold variant; remove old logo file and add light variant

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import logoImg from '@/public/logo.png';
+import logoImg from '@/public/the_wild_embrace_logo_gold.png';
 
 function Logo() {
   return (
